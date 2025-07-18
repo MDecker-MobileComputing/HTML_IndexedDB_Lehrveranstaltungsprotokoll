3f0078f501ac92796427eed69edfe4967ecb9bab
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-07-18 21:27:15 +0200

    datenbank.js: neue Funktion holeDatenbankVerbindung()

--- docs/datenbank.js
+++ docs/datenbank.js
@@ -7,52 +7,46 @@ const DATENBANK_NAME            = "LehrveranstaltungsProtokoll";
 const STORE_LEHRVERANSTALTUNGEN = "lehrveranstaltungen";
 const STORE_PROTOKOLLEINTRAG    = "protokolleintrag";
 
-// Öffnen der IndexedDB-Datenbank anfordern
-const idbOpenRequest = window.indexedDB.open( DATENBANK_NAME, 1 );
-
 
 /**
- * Event-Handler für das Upgrade der Datenbank; wird auch aufgerufen,
- * wenn die Datenbank zum ersten Mal erstellt wird.
- * Hier werden die Object Stores erstellt, die für die Anwendung benötigt werden
- * (diese Objects Stores sind vergleichbar mit Tabellen in relationalen Datenbanken).
+ * Verbindungsobjekt der Datenbank holen (Datenbank wird ggf. erstellt).
+ * 
+ * @returns {Promise<IDBDatabase>} Promise auf Verbindungsobjekt der Datenbank
  */
-idbOpenRequest.onupgradeneeded = function( event ) {
+function holeDatenbankVerbindung() {
 
-    const db = event.target.result;
+  return new Promise( (resolve, reject ) => {
 
-    if ( !db.objectStoreNames.contains( STORE_LEHRVERANSTALTUNGEN ) ) {
+    const idbOpenRequest = window.indexedDB.open( DATENBANK_NAME, 1 );
+
+    idbOpenRequest.onupgradeneeded = (event) => {
+
+      const db = event.target.result;
+      if ( !db.objectStoreNames.contains( STORE_LEHRVERANSTALTUNGEN ) ) {
 
         db.createObjectStore( STORE_LEHRVERANSTALTUNGEN, { keyPath: "id", autoIncrement: true } );
-        console.log( `Object Store \"${STORE_LEHRVERANSTALTUNGEN}\" erstellt.` );
+      }
+      if ( !db.objectStoreNames.contains( STORE_PROTOKOLLEINTRAG ) ) {
 
-    } else {
+        const store = db.createObjectStore( STORE_PROTOKOLLEINTRAG, { keyPath: "id", autoIncrement: true } );
+        store.createIndex( "vorlesungId", "vorlesungId", { unique: false } );
+      }
+    };
 
-        console.log( `Object Store \"${STORE_LEHRVERANSTALTUNGEN}\" existiert bereits.` );
-    }
+    idbOpenRequest.onsuccess = function( ) {
+
+        console.log( `Datenbank \"${DATENBANK_NAME}\" erfolgreich geöffnet.` );
+        return resolve( idbOpenRequest.result );    
+    };
+
+    idbOpenRequest.onerror = function( ) {
+
+        console.error( `Fehler beim Öffnen der Datenbank \"${DATENBANK_NAME}\": ${idbOpenRequest.error}` );
+        reject ( idbOpenRequest.error  );
+    };
+  });
 }
 
-/** Objekt für Zugriff auf geöffnete Datenbank. */
-let datenbank = null;
-
-/**
- * Event-Handler, der aufgerufen wird, wenn die Datenbank erfolgreich geöffnet wurde.
- */
-idbOpenRequest.onsuccess = function( event ) {
-
-    datenbank = event.target.result;
-    console.log( `Datenbank \"${DATENBANK_NAME}\" erfolgreich geöffnet.` );
-}
-
-/**
- * Event-Handler für Fehler beim Öffnen der Datenbank, z.B. wenn Speicherplatz
- * erschöpft ist oder die Datenbankdatei fehlerhaft ist.
- */
-idbOpenRequest.onerror = function( event ) {
-
-    const fehler = event.target.error;
-    console.error( `Fehler beim Öffnen der Datenbank \"${DATENBANK_NAME}\": ${fehler}` );
-}
 
 /**
  * Neue Lehrveranstaltung in Datenbank anlegen.
@@ -63,13 +57,15 @@ idbOpenRequest.onerror = function( event ) {
  */
 async function neueLehrveranstaltung( nameLehrveranstaltung ) {
 
-    return new Promise( ( resolve, reject ) => {
+    const datenbank = await holeDatenbankVerbindung();
+
+      return new Promise( ( resolve, reject ) => {
 
         const tx      = datenbank.transaction( STORE_LEHRVERANSTALTUNGEN, "readwrite" );
         const store   = tx.objectStore( STORE_LEHRVERANSTALTUNGEN );
-        const request = store.add( { name: nameLehrveranstaltung } );
+        const request = store.add({ name: nameLehrveranstaltung });
 
-        request.onsuccess = function() { resolve( request.result ); }; // ID der neu erstellten Lehrveranstaltung zurückgeben
-        request.onerror   = function() { reject(  request.error  ); };
-    });
+        request.onsuccess = () => resolve( request.result );
+        request.onerror   = () => reject(  request.error  );
+      });
 }
